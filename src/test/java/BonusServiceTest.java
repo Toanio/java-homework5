e45905f1e5cac110e6866672d54ec07d573462b7
Чан Тоан <c.toan@corp.mail.ru>
2020-05-12 11:43:24 +0300

    Исправил pom.xml и добавил тесты

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BonusServiceTest {
 
     @Test
-    public void calculates()
+     void shouldCalculatesRegisteredAndInLimit()
     {
         BonusService service=new BonusService();
 
@@ -20,4 +20,61 @@ public class BonusServiceTest {
 
         assertEquals(expected,actual);
     }
+    @Test
+    void shouldCalculateRegisteredAndOverLimit()
+    {
+        BonusService service=new BonusService();
+
+        long amount=1_000_000_60;
+        boolean registered=true;
+
+        long expected=500;
+
+        long actual=service.calculate(amount,registered);
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredAndInLimit()
+    {
+        BonusService service=new BonusService();
+
+        long amount=1000_60;
+        boolean registered=false;
+
+        long expected=10;
+
+        long actual=service.calculate(amount,registered);
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCalculateNotRegisteredAndOverLimit()
+    {
+        BonusService service=new BonusService();
+
+        long amount=1_000_000_60;
+        boolean registered=false;
+
+        long expected=500;
+
+        long actual=service.calculate(amount,registered);
+
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCalculateRegisteredAndAmountZero()
+    {
+        BonusService service=new BonusService();
+
+        long amount=0;
+        boolean registered=false;
+
+        long expected=0;
+
+        long actual=service.calculate(amount,registered);
+
+        assertEquals(expected,actual);
+    }
+
 }
